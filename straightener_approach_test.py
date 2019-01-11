e2f3f8577bc54a8c6c8ceeada7ab5a6d6b287c2a
douglas kelly <douglas.kelly@riken.jp>
2019-01-11 17:41:46 +0900

    Use IJ's Straighten functionality to effectively compensate for effects of sinuous vessel axis. Currently user must trace axes on projections, and run the code twice in succession...

---
+++ straightener_approach_test.py
@@ -0,0 +1,57 @@
+ # @ImagePlus imp
+from ij import ImageStack, ImagePlus, IJ
+from ij.gui import WaitForUserDialog
+from ij.plugin import ChannelSplitter, Straightener, ZProjector
+
+# run on imp containing result of tubefitter + IJ.run(imp, "Reslice [/]...", "output=1.000 start=Top avoid") + spline-fitted 
+# manual roi copied from z-projected version of this resliced image (add roi from z-proj to ROI manager, then add 
+# to resliced image)
+
+imp.killRoi();
+IJ.run(imp, "Reslice [/]...", "output=1.000 start=Top avoid");
+rot_imp = IJ.getImage();
+zproj_imp = ZProjector.run(rot_imp, "max");
+zproj_imp.show();
+IJ.setTool("freeline");
+WaitForUserDialog("Draw a line").show();
+IJ.run(zproj_imp, "Fit Spline", "");
+roi = zproj_imp.getRoi();
+zproj_imp.close();
+
+split_ch = ChannelSplitter().split(rot_imp);
+mch_imp = split_ch[0];
+egfp_imp = split_ch[1];
+roi_imp = split_ch[2];
+
+z_planes = egfp_imp.getNSlices();
+#straight_stack = Straightener().straightenStack(egfp_imp, roi, 100);
+#out_imp = ImagePlus("Straightened", straight_stack);
+
+for zidx in range(z_planes):
+	for chidx in range(3):
+		#print("Working on " + str(["egfp", "mCh", "roi"][chidx]));
+		split_ch[chidx].setZ(zidx+1);
+		split_ch[chidx].setRoi(roi);
+		ip = Straightener().straightenLine(split_ch[chidx], 150);
+		if chidx==1:
+			if zidx==0:
+				egfp_straight_stack = ImageStack(ip.getWidth(), ip.getHeight());
+			egfp_straight_stack.addSlice(ip);
+		elif chidx==0:
+			if zidx==0:
+				mch_straight_stack = ImageStack(ip.getWidth(), ip.getHeight());
+			mch_straight_stack.addSlice(ip);
+		else:
+			if zidx==0:
+				roi_straight_stack = ImageStack(ip.getWidth(), ip.getHeight());
+			roi_straight_stack.addSlice(ip);
+
+egfp_out_imp = ImagePlus("Straightened EGFP", egfp_straight_stack);
+mch_out_imp = ImagePlus("Straightened mCh", mch_straight_stack);
+roi_out_imp = ImagePlus("Straightened ROI", roi_straight_stack);
+egfp_out_imp.show();
+mch_out_imp.show();
+roi_out_imp.show();
+IJ.run("Merge Channels...", "c1=[" + mch_out_imp.getTitle() + 
+									"] c2=[" + egfp_out_imp.getTitle() + 
+									"] c7=[" + roi_out_imp.getTitle() + "] create keep");
