6e1ddd0de696c1870a1654ba38c207d50cab7589
douglas kelly <douglas.kelly@riken.jp>
2019-01-21 18:05:20 +0900

    Do unwrapping before generation of radius image so that fill holes can be used to get desired thresholding result

--- v1.x.x/modules/projection.py
+++ v1.x.x/modules/projection.py
@@ -2,12 +2,13 @@
 import math, sys, os
 from ij import ImageStack, ImagePlus, IJ
 from ij.gui import PolygonRoi, Roi, WaitForUserDialog, Line, ProfilePlot
-from ij.plugin import Duplicator, MontageMaker, ChannelSplitter
-from ij.plugin.filter import MaximumFinder
+from ij.plugin import Duplicator, MontageMaker, ChannelSplitter, ImageCalculator
+#from ij.plugin.filter import MaximumFinder
 from ij.process import FloatProcessor, AutoThresholder
 
-def twist_and_unwrap(imp, rimp):
+def twist_and_unwrap(imp):
 	"""from the output of angular projection, define an axis along which to unzip the vessel and return this unzipped image"""
+	imp.show();
 	IJ.setTool("freeline");
 	WaitForUserDialog("Input required...", "Please draw the line down which unwrapping should occur...").show();
 	roi = imp.getRoi();
@@ -26,21 +27,24 @@ def twist_and_unwrap(imp, rimp):
 		unwrap_poly_ys.insert(0, imp.getHeight());
 		unwrap_poly_ys.append(1);
 	unwrap_axis = [(x, y) for (x, y) in zip(unwrap_poly_xs, unwrap_poly_ys)];
+	
+	unwrapped_projection_imp = do_unwrap(imp, unwrap_axis);
+	return unwrapped_projection_imp, unwrap_axis;
+
+def do_unwrap(imp, unwrap_axis):
+	"""given an unwrap axis extending from top to bottom of an image, generate an unwrapped image"""
 	# extend to right hand corners...
+	print(unwrap_axis)
+	unwrap_poly_xs = [x for x, y in unwrap_axis];
+	unwrap_poly_ys = [y for x, y in unwrap_axis];
 	unwrap_poly_xs.append(imp.getWidth());
 	unwrap_poly_xs.append(imp.getWidth());
 	unwrap_poly_ys.append(unwrap_poly_ys[-1]);
 	unwrap_poly_ys.append(unwrap_poly_ys[0]);
-	new_roi = PolygonRoi(unwrap_poly_xs, unwrap_poly_ys, Roi.POLYGON);
+	unwrap_roi = PolygonRoi(unwrap_poly_xs, unwrap_poly_ys, Roi.POLYGON);
 
-	unwrapped_projection_imp = do_unwrap(imp, new_roi);
-	unwrapped_r_imp = do_unwrap(rimp, new_roi);
-	return unwrapped_projection_imp, unwrapped_r_imp, unwrap_axis;
-
-def do_unwrap(imp, unwrap_roi):
-	"""given a roi enclosing the image on one side of an arbitrary unwrap axis, generate an unwrapped image"""
 	input_title = imp.getTitle();
-	unwrap_imp = IJ.createImage("unwrap", imp.getWidth(), imp.getHeight(), 2, 32);
+	unwrap_imp = IJ.createImage("unwrap", imp.getWidth(), imp.getHeight(), 2, imp.getBitDepth());
 	left_crop = unwrap_roi.getBoundingRect().x;
 	dummy = Duplicator().run(imp);
 	dummy.setRoi(unwrap_roi);
@@ -71,7 +75,9 @@ def do_angular_projection(imp, max_r_pix=60, min_r_pix=10, generate_roi_stack=Fa
 	"""perform ray-based projection of vessel wall, c.f. ICY TubeSkinner (Lancino 2018)"""
 	split_chs = ChannelSplitter().split(imp);
 	mch_imp = split_chs[0];
+	IJ.setAutoThreshold(mch_imp, "IsoData dark stack");
 	egfp_imp = split_chs[1];
+	proj_imp = Duplicator().run(egfp_imp);
 	cl_imp = split_chs[2];
 	if generate_roi_stack:
 		egfp_imp_disp = Duplicator().run(egfp_imp);
@@ -82,14 +88,31 @@ def do_angular_projection(imp, max_r_pix=60, min_r_pix=10, generate_roi_stack=Fa
 	ring_rois = [];
 	for zidx in range(cl_imp.getNSlices()):
 		if ((zidx+1) % 100)==0:
-			print("Progress = " + str(100*(float(zidx+1)/cl_imp.getNSlices())));
+			print("Progress = " + str(round(100*(float(zidx+1)/cl_imp.getNSlices()))));
 		projected_im_row = [];
-		egfp_imp.setZ(zidx+1);
-		cl_imp.setZ(zidx+1);
-		ip = cl_imp.getProcessor();
-		maxpt = MaximumFinder().getMaxima(ip, 10, True)
-		centres.append((maxpt.xpoints[0], maxpt.ypoints[0]));
-		centre = (maxpt.xpoints[0], maxpt.ypoints[0]);
+		proj_imp.setZ(zidx+1);
+		mch_imp.setZ(zidx+1);
+		bp = mch_imp.createThresholdMask();
+		bp.dilate();
+		bp.erode();
+		bp.erode();
+		bp.erode();
+		mask_imp = ImagePlus("mask", bp);
+		IJ.run(mask_imp, "Create Selection", "");
+		roi = mask_imp.getRoi();
+		proj_imp.setRoi(roi);
+		IJ.run(proj_imp, "Set...", "value=0 slice");
+		IJ.run(proj_imp, "Make Inverse", "");
+		roi = proj_imp.getRoi();
+#		print("New method = "  +str((roi.getStatistics().xCentroid, roi.getStatistics().yCentroid)));
+#		cl_imp.setZ(zidx+1);
+#		ip = cl_imp.getProcessor();
+#		maxpt = MaximumFinder().getMaxima(ip, 10, True)
+#		print("Previous method = " + str((maxpt.xpoints[0], maxpt.ypoints[0])));
+#		centres.append((maxpt.xpoints[0], maxpt.ypoints[0]));
+#		centre = (maxpt.xpoints[0], maxpt.ypoints[0]);
+		centre = (roi.getStatistics().xCentroid, roi.getStatistics().yCentroid);
+		centres.append(centre);
 		ring_roi_xs = [];
 		ring_roi_ys = [];
 		for theta in range(360):
@@ -98,7 +121,7 @@ def do_angular_projection(imp, max_r_pix=60, min_r_pix=10, generate_roi_stack=Fa
 			pt2 = (centre[0] + max_r_pix * math.cos(math.radians(theta)), 
 					centre[1] + max_r_pix * math.sin(math.radians(theta)));
 			roi = Line(pt1[0], pt1[1], pt2[0], pt2[1]);
-			egfp_imp.setRoi(roi);
+			proj_imp.setRoi(roi);
 			profile = roi.getPixels();
 			projected_im_row.append(max(profile));
 			try:
@@ -109,7 +132,7 @@ def do_angular_projection(imp, max_r_pix=60, min_r_pix=10, generate_roi_stack=Fa
 				ring_roi_ys.append(roi.getContainedPoints()[profile.index(max(profile))].y);
 			except IndexError:
 				ring_roi_ys.append(pt2[1]);
-			egfp_imp.killRoi();
+			proj_imp.killRoi();
 		ring_roi = PolygonRoi(ring_roi_xs, ring_roi_ys, Roi.FREELINE);
 		ring_rois.append(ring_roi);
 		if generate_roi_stack:
@@ -120,11 +143,10 @@ def do_angular_projection(imp, max_r_pix=60, min_r_pix=10, generate_roi_stack=Fa
 		#egfp_imp.setRoi(ring_roi);
 		projected_im_pix.append(projected_im_row);
 		
-	for ch in split_chs:
-		ch.close();
+#	for ch in split_chs:
+#		ch.close();
 
 	out_imp = ImagePlus("projected", FloatProcessor([list(x) for x in zip(*projected_im_pix)]));
-	out_imp.show();
 
 	if generate_roi_stack:
 		roi_stack.show();
@@ -157,7 +179,7 @@ def calculate_mean_r(imp, ring_rois, centres):
 	mask_imp.close();
 	return mean_r;
 
-def generate_r_image(imp, ring_rois, centres):
+def generate_r_image(imp, ring_rois, centres, unwrap_axis):
 	"""for each point in the projection, calculate the distance to the vessel axis and present as an image"""
 	IJ.setAutoThreshold(imp, "Intermodes dark");
 	bp = imp.createThresholdMask();
@@ -165,17 +187,22 @@ def generate_r_image(imp, ring_rois, centres):
 	bp.erode();
 
 	mask_imp = ImagePlus("Mask", bp);
+	mask_imp.show();
+	IJ.run(mask_imp, "Fill Holes", "");
+	mask_imp = do_unwrap(mask_imp, unwrap_axis);
+	IJ.run(mask_imp, "Divide...", "value=255");
 
 	r_list = [];
 	for lidx, (roi, centre) in enumerate(zip(ring_rois, centres)):
-		r_sublist = [math.sqrt((x - centre[0])**2 + (y - centre[1])**2) if bp.get(theta, lidx) > 128 else 0 for theta, (x, y) in enumerate(zip(roi.getPolygon().xpoints, roi.getPolygon().ypoints))];
+		r_sublist = [math.sqrt((x - centre[0])**2 + (y - centre[1])**2) for x, y in zip(roi.getPolygon().xpoints, roi.getPolygon().ypoints)];
 		r_list.append(r_sublist);
 		
-	rimp = ImagePlus("Radii", FloatProcessor([list(x) for x in zip(*r_list)]));
-	rimp.show();
-	IJ.run(rimp, "Cyan Hot", "");
+	r_imp = ImagePlus("Radii", FloatProcessor([list(x) for x in zip(*r_list)]));
+	r_imp = do_unwrap(r_imp, unwrap_axis);
+	r_imp = ImageCalculator().run("Multiply create", r_imp, mask_imp);
+	IJ.run(r_imp, "Cyan Hot", "");
 
-	return rimp, mask_imp
+	return r_imp, mask_imp
 
 def calculate_area_and_aspect_ratio(imp, mean_vessel_r, raw_voxel_side):
 	"""return the area and aspect ratio of the cell based on the projected image and convert to real-world units"""
@@ -234,10 +261,13 @@ def do_slicewise_unwrap(imp):
 
 	
 out_imp, _, ring_rois, centres = do_angular_projection(imp, generate_roi_stack=True);
-r_imp, cell_mask = generate_r_image(out_imp, ring_rois, centres);
-unwrapped_projection_imp, unwrapped_r_imp, unwrap_axis = twist_and_unwrap(out_imp, r_imp);
+unwrapped_projection_imp, unwrap_axis = twist_and_unwrap(out_imp);
+r_imp, cell_mask = generate_r_image(out_imp, ring_rois, centres, unwrap_axis);
+
+r_imp.show();
+out_imp.close();
 unwrapped_projection_imp.show();
-unwrapped_r_imp.show();
+
 
 #for theta, (x, y) in enumerate(zip(ring_roi.getPolygon().xpoints, ring_roi.getPolygon().ypoints)):
 #	print("theta = " + str(theta));
