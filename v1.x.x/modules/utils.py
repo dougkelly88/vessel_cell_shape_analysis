04a8aa0fbd5ef49ab92b5176c7cf7d4becf5c1b8
douglas kelly <douglas.kelly@riken.jp>
2019-01-23 16:28:18 +0900

    Keep pixel physical dimensions in line with rotations

--- v1.x.x/modules/utils.py
+++ v1.x.x/modules/utils.py
@@ -16,28 +16,37 @@ def rot3d(imp, axis='x'):
 	if imp.getType()==ImagePlus.COLOR_256 or imp.getType()==ImagePlus.COLOR_RGB:
 		raise NotImplementedError("Handling of colour images isn't implemented yet");
 	title = imp.getTitle();
+	original_cal = imp.getCalibration();
+	new_cal = original_cal;
 	if axis=='x':
 		input_stack = imp.getStack();
 		output_stack = rot_around_x(input_stack);
-		out_imp = ImagePlus(title, output_stack)
+		out_imp = ImagePlus(title, output_stack);
+		new_cal.pixelHeight = original_cal.pixelDepth;
+		new_cal.pixelDepth = original_cal.pixelHeight;
 	elif axis=='y':
 		input_stack = StackProcessor(imp.getStack()).rotateRight();
 		output_stack = rot_around_x(input_stack);
 		final_stack = StackProcessor(output_stack).rotateRight();
 		out_imp = ImagePlus(title, final_stack);
+		new_cal.pixelWidth = original_cal.pixelDepth;
+		new_cal.pixelDepth = original_cal.pixelWidth;
 	elif axis=='z':
 		output_stack = StackProcessor(imp.getStack()).rotateRight();
-		out_imp = ImagePlus(title, output_stack)
+		out_imp = ImagePlus(title, output_stack);
+		new_cal.pixelWidth = original_cal.pixelHeight;
+		new_cal.pixelHeight = original_cal.pixelWidth;
 	else:
 		raise NotImplementedError("Please check which axis you've chosen - if not (x, y, z) than it's not implemented...");
 	imp.close();
+	out_imp.setCalibration(new_cal);
 	return out_imp;
 
 path = "C:\\Users\\dougk\\Desktop\\test image.tif";
 imp = IJ.openImage(path);
 imp.show();
 
-axis = 'y'
+axis = 'z'
 out_imp = rot3d(imp, axis=axis);
 out_imp.show();
 WaitForUserDialog("rotated around " + axis).show();
