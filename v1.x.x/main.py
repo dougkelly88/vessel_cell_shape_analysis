1c1e6ecd5eb250100721cb2e0472e04155e2771c
douglas kelly <douglas.kelly@riken.jp>
2019-02-12 16:00:20 +0900

    Add crude time profiling

--- v1.x.x/main.py
+++ v1.x.x/main.py
@@ -1,4 +1,5 @@
 import os, sys
+from datetime import datetime
 from ij import IJ, Prefs
 
 release = False;
@@ -23,22 +24,40 @@ im_path = "D:\\data\Marcksl1 cell shape analysis\\2018-12-31 Lumen stained sampl
 metadata_path = "D:\\data\Marcksl1 cell shape analysis\\2018-12-31 Lumen stained samples\\AB inj marcksl1b-EGFP, rhodamine-dextran uangiography\\Cropped\\2019-01-10 16-03-47 output\\Cropped Dextran-Rhodamine Marcksl1b-EGFP Lumen staining test eE1 xISV 1.json";
 output_path = "D:\\data\Marcksl1 cell shape analysis\\2018-12-31 Lumen stained samples\\straightening output";
 
+print("starting tube fitting for vessel axis extraction...");
+t1 = datetime.now();
+tt1 = t1;
 imp = tbf.do_tubefitting(im_path, metadata_path, output_path);
-print("done tubefitting");
+t2 = datetime.now();
+print("done tubefitting in {} s. ".format(round(0.001*(t2.getTime() - t1.getTime()))));
 print(imp);
 Prefs.blackBackground = True;
+print("starting angular projection of vessel surface...");
+t1 = datetime.now();
 out_imp, _, ring_rois, centres = prj.do_angular_projection(imp, generate_roi_stack=True);
-print("done projection")
+t2 = datetime.now();
+print("done projection in {} s. ".format(round(0.001*(t2.getTime() - t1.getTime()))));
+print("starting unwrapping of cell from around vessel wall...");
+t1 = datetime.now();
 IJ.setAutoThreshold(out_imp, "Intermodes dark");
 threshold_val = out_imp.getProcessor().getMinThreshold();
 unwrapped_projection_imp, unwrap_axis = prj.twist_and_unwrap(out_imp);
-print("done unwrapping")
+t2 = datetime.now();
+print("done unwrapping is {} s. ".format(round(0.001*(t2.getTime() - t1.getTime()))));
+print("start generation of image colormapped by radius...");
+t1 = datetime.now();
 r_imp, mask_imp = prj.generate_r_image(out_imp, ring_rois, centres, unwrap_axis, threshold_val);
-print("done generating r image")
+t2 = datetime.now();
+print("done generating r image in {} s. ".format(round(0.001*(t2.getTime() - t1.getTime()))));
 r_imp.show();
 out_imp.close();
 unwrapped_projection_imp.show();
+print("start calculation of cell inner surface area and aspect ratio...");
+t1 = datetime.now();
 area, aspect_ratio = prj.calculate_area_and_aspect_ratio(r_imp, mask_imp, 0.108333);
-print("done calculating outputs")
+t2 = datetime.now();
+print("done calculating outputs in {} s. ".format(round(0.001*(t2.getTime() - t1.getTime()))));
 print("A = " + str(area));
 print("Aspect ratio = " + str(aspect_ratio));
+t2 = datetime.now();
+print("Finished in {} s. ".format(round(0.001*(t2.getTime() - tt1.getTime()))));
